58df351ff473d69e0c93a918fcc982a2f4d0424a
Nikita <n.shvedov27@gmail.com>
2024-06-29 15:13:22 +0500

    Initial Commit

---
+++ ValentinesDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ValentinesDay
+//
+//  Created by Nikita Shvedov on 29.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
